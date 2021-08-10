e1ce7409f28e4836798ac4e91a46a40379b12adf
蒲茂超 <1442169607@qq.com>
2021-08-10 21:55:19 +0800

    创建结构

--- platform/platform-getway/src/main/java/com/platform/cloud/getway/config/ConfigKnife4j.java
+++ platform/platform-getway/src/main/java/com/platform/cloud/getway/config/ConfigKnife4j.java
@@ -1,7 +1,18 @@
 package com.platform.cloud.getway.config;
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.*;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,24 +24,24 @@ import java.util.List;
  */
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class ConfigKnife4j{
     @Autowired
     private ApplicationContext applicationContext;
 
     @Bean(value = "defaultApi2")
     public Docket defaultApi2(){
-        Docket docket = new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).groupName("网关").select().apis(RequestHandlerSelectors.withClassAnnotation(Api.class)).paths(PathSelectors.any()).build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).groupName("网关").select().apis(RequestHandlerSelectors.withClassAnnotation(Api.class)).paths(PathSelectors.any()).build().securitySchemes(securitySchemes());
         return docket;
     }
 
     // 生成接口信息
     private ApiInfo apiInfo(){
-        return new ApiInfoBuilder().title("platform网关").description(applicationContext.getEnvironment().getProperty("project.name")).version(applicationContext.getEnvironment().getProperty("project.version")).build().securitySchemes(securitySchemes());
+        return new ApiInfoBuilder().title("platform网关").description(applicationContext.getEnvironment().getProperty("project.name")).version(applicationContext.getEnvironment().getProperty("project.version")).build();
     }
 
     private List<ApiKey> securitySchemes(){
-        java.util.List<ApiKey> apiKeyList = new ArrayList<>();
+        List<ApiKey> apiKeyList = new ArrayList<>();
         apiKeyList.add(new ApiKey("Authorization","认证参数","header"));
         return apiKeyList;
     }
