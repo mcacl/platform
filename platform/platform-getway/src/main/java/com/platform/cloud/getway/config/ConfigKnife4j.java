d378e10a1347721c320172239821d3d77c2006c0
pumaochao <pumaochao@58hn.cn>
2021-08-11 17:58:22 +0800

    创建结构

--- platform/platform-getway/src/main/java/com/platform/cloud/getway/config/ConfigKnife4j.java
+++ platform/platform-getway/src/main/java/com/platform/cloud/getway/config/ConfigKnife4j.java
@@ -3,19 +3,21 @@ package com.platform.cloud.getway.config;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import org.springframework.context.annotation.*;
-
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 创建时间:2021/7/31
@@ -40,9 +42,9 @@ public class ConfigKnife4j{
         return new ApiInfoBuilder().title("platform网关").description(applicationContext.getEnvironment().getProperty("project.name")).version(applicationContext.getEnvironment().getProperty("project.version")).build();
     }
 
-    private List<ApiKey> securitySchemes(){
+    private List<SecurityScheme> securitySchemes(){
         List<ApiKey> apiKeyList = new ArrayList<>();
         apiKeyList.add(new ApiKey("Authorization","认证参数","header"));
-        return apiKeyList;
+        return apiKeyList.stream().collect(Collectors.toList());
     }
 }
--- platform/platform-getway/src/test/java/getway/Test.java
+++ platform/platform-getway/src/test/java/getway/Test.java
@@ -1,12 +1,20 @@
 package getway;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import springfox.documentation.service.ApiKey;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class Test{
 
-    @Test
+    @org.junit.jupiter.api.Test
     void contextLoads(){
+        List<ApiKey> apiKeyList = new ArrayList<>();
+        apiKeyList.add(new ApiKey("Authorization","认证参数","header"));
+        return apiKeyList.stream().collect(Collectors.toList());
     }
 
 }
