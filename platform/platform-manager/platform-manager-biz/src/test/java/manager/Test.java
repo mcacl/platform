eb8e27babb342bf3a857fe68ce64c2ba22eda196
蒲茂超 <1442169607@qq.com>
2021-08-31 22:18:56 +0800

    创建结构

--- platform/platform-manager/platform-manager-biz/src/test/java/manager/Test.java
+++ platform/platform-manager/platform-manager-biz/src/test/java/manager/Test.java
@@ -1,15 +1,13 @@
 package manager;
 
 import com.platform.cloud.ManagerApplication;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = ManagerApplication.class)
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 public class Test{
 
-    @org.junit.Test
+    @org.junit.jupiter.api.Test
     void contextLoads(){
     }
 
