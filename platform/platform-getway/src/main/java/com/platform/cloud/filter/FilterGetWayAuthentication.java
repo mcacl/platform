c3ab5ca29f68ba5ae796732d15c44aaa102be3bf
pumaochao <pumaochao@58hn.cn>
2021-09-15 18:02:58 +0800

    创建结构

---
+++ platform/platform-getway/src/main/java/com/platform/cloud/filter/FilterGetWayAuthentication.java
@@ -0,0 +1,68 @@
+package com.platform.cloud.filter;
+
+import com.platform.cloud.properties.PropertisGetWay;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/**
+ * 创建时间:2021/9/15 0015
+ * 创建人:pmc
+ * 描述:
+ */
+@Slf4j
+@Component
+public class FilterGetWayAuthentication implements GlobalFilter, Ordered{
+    private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
+    private final PropertisGetWay propertisGetWay;
+
+    public FilterGetWayAuthentication(PropertisGetWay propertisGetWay){
+        this.propertisGetWay = propertisGetWay;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange,GatewayFilterChain chain){
+        String curUrl = exchange.getRequest().getPath().value();
+        List<String> authheaders = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
+        //配置可通过的url
+        List<String> exUrl = propertisGetWay.getSecurity().getExcludeUrls();
+        boolean chenckUrl = exUrl.stream().anyMatch(url->{
+            boolean match = antPathMatcher.match(url,curUrl);
+            if(match){
+                log.info("当前路由:{}匹配到:{}配置规则",curUrl,url);
+            }
+            return match;
+        });
+        if(chenckUrl){
+            return chain.filter(exchange);//通过
+        }
+        if(null == authheaders || authheaders.isEmpty()){
+            return this.response401(exchange);//无请求头
+        }else{
+            String token = authheaders.get(0);
+            //todo 未知内容
+            return this.response401(exchange);
+        }
+    }
+
+    @Override
+    public int getOrder(){
+        return HIGHEST_PRECEDENCE + 1;
+    }
+
+    private Mono<Void> response401(ServerWebExchange exchange){
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.setComplete();
+    }
+}
---
+++ platform/platform-getway/src/main/java/com/platform/cloud/properties/PropertisGetWay.java
@@ -0,0 +1,37 @@
+package com.platform.cloud.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 创建时间:2021/9/15 0015
+ * 创建人:pmc
+ * 描述:
+ */
+@Data
+@Component
+@RefreshScope
+@ConfigurationProperties(prefix = "gateway")
+public class PropertisGetWay{
+    private PropertisTrace trace;
+    private PropertisSecurity security;
+
+    @Data
+    public static class PropertisTrace{
+        private Boolean enable = true;
+    }
+
+    @Data
+    public static class PropertisSecurity{
+        /**
+         * 不过滤的路由
+         */
+        private List<String> excludeUrls = new ArrayList<>();
+        private String key;
+    }
+}
