5ae6d17071684006f71e19429389544d4396f6ea
pumaochao <pumaochao@58hn.cn>
2021-09-18 11:45:26 +0800

    创建结构

---
+++ platform/platform-common/platform-common-core/src/main/java/com/platform/cloud/common/core/enums/EnumApplications.java
@@ -0,0 +1,41 @@
+package com.platform.cloud.common.core.enums;
+
+import lombok.Getter;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 创建时间:2021/9/18 0018
+ * 创建人:pmc
+ * 描述:
+ */
+@Getter
+public enum EnumApplications{
+    /**
+     * 小程序
+     */
+    APPLETS(EnumApplications.NAME_APPLETS,"APP"),
+    /**
+     * 网关
+     */
+    GETWAY(EnumApplications.NAME_GETWAY,"GTW"),
+    /**
+     * 后台管理
+     */
+    MANAGER(EnumApplications.NAME_MANAGER,"MGR");
+
+    public static final Map<String,EnumApplications> APPLICATIONS_MAP = Stream.of(values()).collect(Collectors.toMap(EnumApplications::getApplicationName,e->e));
+    public static final String CLOUND_NAME = "platform";
+    public static final String NAME_APPLETS = "platform-applets-biz";
+    public static final String NAME_GETWAY = "platform-getway-biz";
+    public static final String NAME_MANAGER = "platform-manager-biz";
+    private String applicationName;
+    private String prifix;
+
+    EnumApplications(String applicationName,String prifix){
+        this.applicationName = applicationName;
+        this.prifix = prifix;
+    }
+}
--- platform/platform-getway/src/main/java/com/platform/cloud/getway/config/swagger/ConfigSwagger.java
+++ platform/platform-getway/src/main/java/com/platform/cloud/getway/config/swagger/ConfigSwagger.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -23,7 +24,8 @@ import java.util.List;
  * 创建人:pmc
  * 描述:
  */
-
+//限制环境dev或test可用swagger网关
+@Profile({"dev","test"})
 @Configuration
 @EnableSwagger2
 public class ConfigSwagger{
