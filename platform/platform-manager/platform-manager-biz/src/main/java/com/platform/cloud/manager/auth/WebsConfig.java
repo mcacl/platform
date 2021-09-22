ebac2e17ba5a05e05740aa410923662e724d20f8
蒲茂超 <1442169607@qq.com>
2021-09-22 22:03:29 +0800

    创建结构

---
+++ platform/platform-manager/platform-manager-biz/src/main/java/com/platform/cloud/manager/auth/WebsConfig.java
@@ -0,0 +1,10 @@
+package com.platform.cloud.manager.auth;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 创建时间:2021/9/22
+ * 创建人:pmc
+ * 描述:
+ */
+public class WebsConfig extends WebSecurityConfigurerAdapter{}
