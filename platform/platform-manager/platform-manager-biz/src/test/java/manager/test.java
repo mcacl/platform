ab88362c4b9fe278a892daf85779c4f5c4d80fd1
pumaochao <pumaochao@58hn.cn>
2021-09-29 10:11:20 +0800

    创建结构

--- platform/platform-manager/platform-manager-biz/src/test/java/manager/test.java
+++ platform/platform-manager/platform-manager-biz/src/test/java/manager/test.java
@@ -2,13 +2,14 @@ package manager;
 
 import com.platform.cloud.ManagerApplication;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = ManagerApplication.class)
 //@RunWith(SpringRunner.class)
-public class Test{
+public class test{
 
-    @org.junit.jupiter.api.Test
+    @Test
     void contextLoads(){
         String codeStr = String.valueOf(2);
         System.out.println(StringUtils.leftPad(codeStr,5,"0"));
