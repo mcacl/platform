64df4577724092ab90c616c2eae9b47a24e40ec5
蒲茂超 <1442169607@qq.com>
2022-01-18 20:02:27 +0800

    基础

---
+++ platform/platform-source/biz/com/platform/cloud/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.platform.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class BaseApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(BaseApplication.class,args);
+    }
+
+}
---
+++ platform/platform-source/biz/com/platform/cloud/base1/mapper/mapper.java
@@ -0,0 +1,8 @@
+package com.platform.cloud.base1.mapper;
+
+/**
+ * 创建时间:2021/11/13
+ * 创建人:pmc
+ * 描述:
+ */
+public class mapper{}
